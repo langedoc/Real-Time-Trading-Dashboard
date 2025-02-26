a1cf2753234b28c14d7544887ac5aa3e0fff3862
yulia <langedoc@gmail.com>
2025-02-26 15:46:54 +0100

    feat: Chart component showing historical data implemented

--- src/app/components/dashboard/PriceChart/PriceChart.jsx
+++ src/app/components/dashboard/PriceChart/PriceChart.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useCrypto } from "@/app/context/CryptoContext";
+import Chart from "@/app/components/dashboard/PriceChart/Chart";
 
 export default function PriceChart() {
     const { selectedPair } = useCrypto();
@@ -22,20 +23,7 @@ export default function PriceChart() {
 
     return (
         <div>
-            {ohlcvData.length > 0 && (
-                <ul>
-                    {ohlcvData.map((candle, index) => (
-                        <li key={index}>
-                            Time: {new Date(candle.time).toLocaleString()}, 
-                            Open: {candle.open}, 
-                            High: {candle.hight}, 
-                            Low: {candle.low}, 
-                            Close: {candle.close}, 
-                            Volume: {candle.volume}
-                        </li>
-                    ))}
-                </ul>
-            )}
+            {ohlcvData.length > 0 && <Chart key={selectedPair} ohlcvData={ohlcvData} />}
         </div>
     );
 };
