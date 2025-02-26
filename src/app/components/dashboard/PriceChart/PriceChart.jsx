30539e6a654efa1bf445c65d3799f67b76dc3fdf
yulia <langedoc@gmail.com>
2025-02-26 11:11:16 +0100

    feat: trading pairs dropdown menu added

--- src/app/components/dashboard/PriceChart/PriceChart.jsx
+++ src/app/components/dashboard/PriceChart/PriceChart.jsx
@@ -18,7 +18,7 @@ export default function PriceChart() {
         };
 
         fetchData();
-    }, []);
+    }, [ selectedPair ]);
 
     return (
         <div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import TraidingPairsMenu from "./components/dashboard/TraidingPairsMenu/TraidingPairsMenu";
 import PriceChart from "./components/dashboard/PriceChart/PriceChart";
 
 export default function Home() {
   return (
     <div>
       <main>
+        <TraidingPairsMenu />
         <PriceChart />
       </main>
       <footer>
