bd45792405ff5b7d0a16325c1f3e243360be1d2a
yulia <langedoc@gmail.com>
2025-02-28 18:51:12 +0100

    WebSocket reconnections implemented

--- src/app/context/useWebSocketOB.js
+++ src/app/context/useWebSocketOB.js
@@ -3,33 +3,39 @@ import { useEffect, useState, useRef } from "react";
 const useWebSocketOB = (symbol) => {
     const [ orderBook, setOrderBook ] = useState(null);
     const wsRef = useRef(null);
+    const reconnectDelay = 1000; // Delay in 1 second before attempting to reconnect
 
     useEffect(() => {
         if(!symbol) return;
 
-        const ws = new WebSocket(`wss://fstream.binance.com/ws/${symbol.toLowerCase()}@depth@500ms`); // Can be adjusted to 250 milliseconds, 500 milliseconds, 100 milliseconds (if existing)
-        wsRef.current = ws;
+        const connectWebSocket = () => {
+            const ws = new WebSocket(`wss://fstream.binance.com/ws/${symbol.toLowerCase()}@depth@500ms`);
+            wsRef.current = ws;
 
-        ws.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            setOrderBook({
-                // Showing only first 10 for best readability and performance
-                bids: data.b.slice(0, 10).map(([price, quantity]) => ({price: parseFloat(price), quantity: parseFloat(quantity) })),
-                asks: data.a.slice(0, 10).map(([price, quantity]) => ({price: parseFloat(price), quantity: parseFloat(quantity) })),
-            });
+            ws.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                setOrderBook({
+                    // Showing only first 10 for best readability and performance
+                    bids: data.b.slice(0, 10).map(([price, quantity]) => ({price: parseFloat(price), quantity: parseFloat(quantity) })),
+                    asks: data.a.slice(0, 10).map(([price, quantity]) => ({price: parseFloat(price), quantity: parseFloat(quantity) })),
+                });
+            };
+
+            ws.onerror = (error) => {
+                console.error("WebSocket error:", error);
+                setOrderBook(null);
+            };
+
+            ws.onclose = () => {
+                console.warn("WebSocket connection closed. Attempting to reconnect...");
+                setOrderBook(null);
+                setTimeout(connectWebSocket, reconnectDelay); // Attempt to reconnect
+            };
         };
 
-        ws.onerror = (error) => {
-            console.error("WebSocket error:", error);
-            setOrderBook(null);
-        };
+        connectWebSocket(); // Initial connection
 
-        ws.onclose = () => {
-            console.warn("WebSocket connection closed. Attempting to reconnect...");
-            setOrderBook(null);
-        };
-
-        return () => ws.close();
+        return () => wsRef.current && wsRef.current.close();
     }, [symbol]);
     
     return { orderBook };
--- src/app/context/useWebSocketPC.js
+++ src/app/context/useWebSocketPC.js
@@ -7,30 +7,41 @@ const useWebSocketPC = (symbol) => {
     useEffect(() => {
         if(!symbol) return;
 
-        const ws = new WebSocket(`wss://fstream.binance.com/ws/${symbol.toLowerCase()}@kline_1h`);
-        wsRef.current = ws;
+        const connectWebSocket = () => {
 
-        ws.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            if (data.e === "kline") {
-                const kline = data.k;
-                setOhlcvData({
-                    time: kline.t,
-                    open: parseFloat(kline.o),
-                    high: parseFloat(kline.h),
-                    low: parseFloat(kline.l),
-                    close: parseFloat(kline.c),
-                    volume: parseFloat(kline.v),
-                });
-            }
+            const ws = new WebSocket(`wss://fstream.binance.com/ws/${symbol.toLowerCase()}@kline_1h`);
+            wsRef.current = ws;
+
+            ws.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                if (data.e === "kline") {
+                    const kline = data.k;
+                    setOhlcvData({
+                        time: kline.t,
+                        open: parseFloat(kline.o),
+                        high: parseFloat(kline.h),
+                        low: parseFloat(kline.l),
+                        close: parseFloat(kline.c),
+                        volume: parseFloat(kline.v),
+                    });
+                }
+            };
+
+            ws.onerror = (error) => {
+                console.error("WebSocket error:", error);
+                setOhlcvData(null);
+            };
+
+            ws.onclose = () => {
+                console.warn("WebSocket connection closed. Attempting to reconnect...");
+                setOhlcvData(null);
+                setTimeout(connectWebSocket, reconnectDelay); // Attempt to reconnect
+            };
         };
 
-        ws.onerror = (error) => {
-            console.error("WebSocket error:", error);
-            setOhlcvData(null);
-        };
+        connectWebSocket(); // Initial connection
 
-        return () => ws.close();
+        return () => wsRef.current && wsRef.current.close();
     }, [ symbol ]);
 
     return { ohlcvData };
